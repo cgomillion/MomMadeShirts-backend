e99d190526dcceb33a2e3f48bc17984fa6c73c2b
cgomillion <cgomillion2@gmail.com>
2021-05-24 17:13:54 -0400

    got the react frontend to pull from the server and delete

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,81 +3,81 @@ const products = express.Router();
 const productModel = require('../models/productModel');
 // const userModel = require('../models/userModel');
 
-// // seed route
-// products.get('/seed', (req, res) => {
-// 	productModel.create([
-// 		{
-//             type: 'tshirt',
-//             product: {
-//                 name: 'test shirt one', 
-//                 brand: 'my brand', 
-//                 color: 'red',   
-//                 size: 'large', 
-//                 img: 'url.com',
-//                 description: 'test shirt desc.',
-//                 price: 10
-//             }
-// 		},
-// 		{
-//             type: 'sweatshirt',
-//             product: {
-//                 name: 'test sweatshirt one', 
-//                 brand: 'my brand', 
-//                 color: 'yellow',   
-//                 size: 'x-large', 
-//                 img: 'url.com',
-//                 description: 'test sweatshirt desc.',
-//                 price: 15
-//             }
-// 		},
-// 		{
-//             type: 'tanktop',
-//             product: {
-//                 name: 'test tank top one', 
-//                 brand: 'my brand', 
-//                 color: 'brown',   
-//                 size: 'medium', 
-//                 img: 'url.com',
-//                 description: 'test tank top desc.',
-//                 price: 8
-//             }
-// 		},
-//         {
-//             type: 'hoodie',
-//             product: {
-//                 name: 'test hoodie one', 
-//                 brand: 'my brand', 
-//                 color: 'green',   
-//                 size: 'small', 
-//                 img: 'url.com',
-//                 description: 'test hoodie desc.',
-//                 price: 20
-//             }
-// 		},
-//     ],
-// 	(err, data) => {
-//     if(err) {
-//       console.log(err)
-//     } else {
-//     res.send(data)
-//     console.log('seed data created')
-//     }
-//   })
-// })
+// seed route
+products.get('/seed', (req, res) => {
+	productModel.create([    
+		{
+            type: 'tshirt',
+            product: {
+                name: 'test shirt one', 
+                brand: 'my brand', 
+                color: 'red',   
+                size: 'large', 
+                img: 'url.com',
+                description: 'test shirt desc.',
+                price: 10
+            }
+		},
+		{
+            type: 'sweatshirt',
+            product: {
+                name: 'test sweatshirt one', 
+                brand: 'my brand', 
+                color: 'yellow',   
+                size: 'x-large', 
+                img: 'url.com',
+                description: 'test sweatshirt desc.',
+                price: 15
+            }
+		},
+		{
+            type: 'tanktop',
+            product: {
+                name: 'test tank top one', 
+                brand: 'my brand', 
+                color: 'brown',   
+                size: 'medium', 
+                img: 'url.com',
+                description: 'test tank top desc.',
+                price: 8
+            }
+		},
+        {
+            type: 'hoodie',
+            product: {
+                name: 'test hoodie one', 
+                brand: 'my brand', 
+                color: 'green',   
+                size: 'small', 
+                img: 'url.com',
+                description: 'test hoodie desc.',
+                price: 20
+            }
+		},
+    ],
+	(err, data) => {
+    if(err) {
+      console.log(err)
+    } else {
+    res.send(data)
+    console.log('seed data created')
+    }
+  })
+})
 
 
 // GET (index) list of cars
 products.get('/', (req, res)=>{
-    res.send('Get route is working!!!');
-    
-    // productModel.find({}, (error, foundProducts)=>{
-    //     if (error){
-    //         res.status(400).json(error)
-    //     }
-    //     else{
-    //         res.status(200).json(foundProducts)
-    //     }
-    // })
+    // res.send('Get route is working!!!');
+
+    productModel.find({}, (error, foundProducts)=>{
+        if (error){
+            res.status(400).json(error)
+        }
+        else{
+            res.status(200).json(foundProducts)
+        }
+    })
 })
 
 // POST ROUTE
